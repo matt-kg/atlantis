6b92d0e4be6ae8c45a2d5e9d3089927ddcf8a7dc
matt-kg <mattkg@versent.com.au>
2017-09-22 10:23:50 +1000

    In the process of updating client.go to support go-gitlab

--- server/server.go
+++ server/server.go
@@ -76,7 +76,7 @@ type CommandContext struct {
 
 func NewServer(config ServerConfig) (*Server, error) {
 
-	fmt.Printf("Hello, my Github login is %s & my Gitlab login is %s\n", config.GithubUser, config.GitlabUser)
+	//fmt.Printf("Hello, my Github login is %s & my Gitlab login is %s\n", config.GithubUser, config.GitlabUser)
 
 	// if ~ was used in data-dir convert that to actual home directory otherwise we'll
 	// create a directory call "~" instead of actually using home
